620098a84d42f43156c06b2a25b41758c4abacf6
Luis Art <36245824+luisartg@users.noreply.github.com>
2024-03-03 21:51:47 -0600

    - Configured Tilemaps - Basic crate and barrel prefab - Basic player camera

---
+++ Assets/Scripts/FollowPlayerCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector2 playerPos = transform.position;
+        Camera.main.transform.position =
+            new Vector3(
+                playerPos.x,
+                playerPos.y,
+                Camera.main.transform.position.z);
+    }
+}
