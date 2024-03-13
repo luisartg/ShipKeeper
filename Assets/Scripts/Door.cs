0cf290f41cab928fb0deb44d28edbaf46f159355
Luis Art <36245824+luisartg@users.noreply.github.com>
2024-03-13 17:10:26 -0600

    Level 03 completed. Doors added

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public AudioSource slamdoor;
+    public GameObject doorVisual;
+    public Collider2D doorTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorVisual.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void CloseDoor()
+    {
+        if (!doorVisual.activeSelf)
+        {
+            slamdoor.Play();
+            doorVisual.SetActive(true);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        if (doorVisual.activeSelf)
+        {
+            slamdoor.Play();
+            doorVisual.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            doorTrigger.enabled = false;
+            CloseDoor();
+        }
+    }
+}
---
+++ Assets/Scripts/DoorButton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    public Door DoorTarget;
+
+    private bool onZone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onZone)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DoorTarget.OpenDoor();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            onZone = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            onZone = false;
+        }
+    }
+}
